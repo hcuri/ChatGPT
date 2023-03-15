1612a56e8f6c6ae56a79c57045f664949ad65aa5
Hector Curi <hcuri@me.com>
2023-03-14 18:53:50 -0700

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Hector Curi on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
